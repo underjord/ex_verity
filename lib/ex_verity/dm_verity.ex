0bfb7484c69bc183e6be45061bd4611a7a2c0024
Lars Wikman <lars@underjord.io>
2025-02-12 11:46:09 +0100

    Include root hash and verity offset in boot.img for rpi4

--- lib/ex_verity/dm_verity.ex
+++ lib/ex_verity/dm_verity.ex
@@ -1,5 +1,6 @@
 defmodule ExVerity.DmVerity do
-  def generate_tree!(%{private_key_path: priv_path, public_key_path: pub_path} = config, fs_path) when is_binary(priv_path) and is_binary(pub_path) do
+  def generate_tree!(%{private_key_path: priv_path, public_key_path: pub_path} = config, fs_path)
+      when is_binary(priv_path) and is_binary(pub_path) do
     block_size = Map.get(config, :block_size, 4096)
     root_hash_path = Map.get(config, :root_hash_path, Briefly.create!())
 
@@ -10,7 +11,12 @@ defmodule ExVerity.DmVerity do
         verity_setup_same_file(fs_path, root_hash_path, block_size)
       end
 
-    {File.read!(root_hash_path), data_size}
+    root_hash = File.read!(root_hash_path)
+    fs_path_dir = Path.dirname(fs_path)
+    system_path = config[:system_path] || System.fetch_env!("NERVES_SYSTEM")
+    File.write!(Path.join(system_path, "images/root_hash.txt"), root_hash)
+    File.write!(Path.join(system_path, "images/verity_offset.txt"), to_string(data_size))
+    {root_hash, data_size}
   end
 
   def generate_tree!(config, _) do
@@ -22,16 +28,19 @@ defmodule ExVerity.DmVerity do
 
   defp verity_setup_same_file(fs_path, root_hash_path, block_size) do
     %{size: data_size} = File.stat!(fs_path)
+
     case System.cmd("veritysetup", [
-      "format", fs_path, fs_path,
-      "--data-block-size=#{block_size}",
-      "--hash-block-size=#{block_size}",
-      "--hash-offset=#{data_size}",
-      "--root-hash-file=#{root_hash_path}"
-    ]
-    ) do
+           "format",
+           fs_path,
+           fs_path,
+           "--data-block-size=#{block_size}",
+           "--hash-block-size=#{block_size}",
+           "--hash-offset=#{data_size}",
+           "--root-hash-file=#{root_hash_path}"
+         ]) do
       {_, 0} ->
         {:ok, data_size}
+
       {output, status} ->
         IO.puts("veritysetup exited with status: #{status}")
         IO.puts("output:")
@@ -42,13 +51,15 @@ defmodule ExVerity.DmVerity do
 
   defp verity_setup_separate_file(fs_path, root_hash_path, block_size, hash_file) do
     case System.cmd("veritysetup", [
-      "format", fs_path, hash_file,
-      "--data-block-size=#{block_size}",
-      "--root-hash-file=#{root_hash_path}"
-    ]
-    ) do
+           "format",
+           fs_path,
+           hash_file,
+           "--data-block-size=#{block_size}",
+           "--root-hash-file=#{root_hash_path}"
+         ]) do
       {_, 0} ->
         {:ok, 0}
+
       {output, status} ->
         IO.puts("veritysetup exited with status: #{status}")
         IO.puts("output:")
